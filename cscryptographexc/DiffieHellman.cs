1d8db366d6dc7641c651f06147ec5ac76637ef13
Anton Kostov <tonigreat@abv.bg>
2024-11-04 23:30:15 +0200

    added 5th exc alg

---
+++ cscryptographexc/DiffieHellman.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cscryptographexc
+{
+    internal static class DiffieHellman
+    {
+        static string Encrypted(string input, int[,] nums)
+        {
+            //the final key to be used with the transpos mthd
+            int[] key = new int[nums.GetLength(0)];
+            //fetches all public keys and assembles the final key
+            for (int i = 0; i < nums.GetLength(0); i++)
+            {
+                int aliceKey = Key(new int[] { nums[i, 0], nums[i, 1], nums[i, 2] });
+                int bobKey = Key(new int[] { nums[i, 0], nums[i, 1], nums[i, 3] });
+                if (aliceKey == bobKey) Console.WriteLine("nice ヾ(⌐■_■)ノ♪");
+                else return "l";
+                key[i] = aliceKey;
+            }
+            return Transposition.Transpos(input, key);
+        }
+        //semantics ig
+        static int PrivateKey(int[] nums)
+        {
+            return (int)Math.Pow(nums[1], nums[2]) % nums[0];
+        }
+        static int Key(int[] nums)
+        {
+            return (int)Math.Pow(PrivateKey(nums), nums[2]) % nums[0];
+        }
+    }
+}
